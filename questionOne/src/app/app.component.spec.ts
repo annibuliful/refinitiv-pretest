37a315c91f367a093fb048db12a4aeb9a9fc33ad
lagmanzaza <lagmanmc@gmail.com>
2021-08-04 21:59:34 +0700

    feat: unit testing for question one

--- questionOne/src/app/app.component.spec.ts
+++ questionOne/src/app/app.component.spec.ts
@@ -1,9 +1,20 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+
+const getElementByQuery = (
+  fixture: ComponentFixture<AppComponent>,
+  query: string
+) => {
+  const element = fixture.debugElement.query(By.css(query));
+  return element.nativeElement as HTMLInputElement;
+};
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [FormsModule],
       declarations: [AppComponent],
     }).compileComponents();
   });
@@ -40,5 +51,91 @@ describe('AppComponent', () => {
     expect(compiled.querySelector('#third-column')?.textContent).toEqual(
       'false'
     );
+  });
+
+  it('should return true when value = 3 and option checkPrime', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      fixture.detectChanges();
+
+      const inputNativeElement = getElementByQuery(fixture, 'input');
+      inputNativeElement.value = '3';
+      inputNativeElement.dispatchEvent(new Event('input'));
+      fixture.detectChanges();
+
+      const resultNativeElement = getElementByQuery(fixture, '#third-column');
+      expect(resultNativeElement.textContent).toEqual('true');
+    });
+  });
+
+  it('should return false when value = 4 and option checkPrime', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      fixture.detectChanges();
+
+      const inputNativeElement = getElementByQuery(fixture, 'input');
+      inputNativeElement.value = '4';
+      inputNativeElement.dispatchEvent(new Event('input'));
+      fixture.detectChanges();
+
+      const resultNativeElement = getElementByQuery(fixture, '#third-column');
+      expect(resultNativeElement.textContent).toEqual('false');
+    });
+  });
+
+  it('should return false when value = 4 then change option = checkFibonacci', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      fixture.detectChanges();
+
+      const inputNativeElement = getElementByQuery(fixture, 'input');
+      inputNativeElement.value = '4';
+      inputNativeElement.dispatchEvent(new Event('input'));
+
+      const selectNativeElement = getElementByQuery(
+        fixture,
+        '#second-column > select'
+      );
+      selectNativeElement.value = 'checkFibonacci';
+      selectNativeElement.dispatchEvent(new Event('select'));
+
+      fixture.detectChanges();
+
+      const resultElement = fixture.debugElement.query(By.css('#third-column'));
+      const resultNativeElement = resultElement.nativeElement as HTMLDivElement;
+      expect(resultNativeElement.textContent).toEqual('false');
+      expect(inputNativeElement.value).toEqual('4');
+      expect(selectNativeElement.value).toEqual('checkFibonacci');
+    });
+  });
+
+  it('should return true when value = 2 then change option = checkFibonacci', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      fixture.detectChanges();
+
+      const inputNativeElement = getElementByQuery(fixture, 'input');
+      inputNativeElement.value = '2';
+      inputNativeElement.dispatchEvent(new Event('input'));
+
+      const selectNativeElement = getElementByQuery(
+        fixture,
+        '#second-column > select'
+      );
+      selectNativeElement.value = 'checkFibonacci';
+      selectNativeElement.dispatchEvent(new Event('select'));
+
+      fixture.detectChanges();
+
+      const resultElement = fixture.debugElement.query(By.css('#third-column'));
+      const resultNativeElement = resultElement.nativeElement as HTMLDivElement;
+      expect(resultNativeElement.textContent).toEqual('true');
+      expect(inputNativeElement.value).toEqual('2');
+      expect(selectNativeElement.value).toEqual('checkFibonacci');
+    });
   });
 });
